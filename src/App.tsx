9ffcd54ad806fec7d2273dde4869a5f7cd9b602a
yumekiti <yumekiti120440@gmail.com>
2022-09-23 19:01:43 +0900

    `add` header

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { FC } from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Link } from 'react-router-dom';
 
 import Home from './pages/Home';
 import About from './pages/About';
@@ -8,11 +8,19 @@ import Header from './components/Header';
 const App: FC = () => {
   return (
     <>
-      <Header />
+      <Header
+        title='ゆめきちのブログっぽいなにか'
+        links={[
+          { name: 'Home', path: '/' },
+          { name: 'About', path: '/about' }
+        ]}
+      />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
       </Routes>
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
     </>
   );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,35 @@
 import { FC } from 'react';
+import { Link } from 'react-router-dom';
 
-const Header: FC = () => {
-  return <div>Header</div>;
+interface Props {
+  title: string;
+  links: {
+    name: string;
+    path: string;
+  }[];
+}
+
+const Header: FC<Props> = ({ title, links }) => {
+  return (
+    <header>
+      <div className="flex w-full items-center bg-white">
+        <Link to='/' className='p-4'>
+          {title}
+        </Link>
+        <nav className='flex-grow flex justify-end'>
+          {links.map((link) => (
+            <Link
+              key={link.path}
+              to={link.path}
+              className='p-4'
+            >
+              {link.name}
+            </Link>
+          ))}
+        </nav>
+      </div>
+    </header>
+  )
 }
 
 export default Header;
