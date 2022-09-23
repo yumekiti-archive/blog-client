70dd9ad2ad73e3180351fe8dbb379b39ae82a0e3
yumekiti <yumekiti120440@gmail.com>
2022-09-23 22:03:23 +0900

    `add` Tags

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { FC } from 'react';
 
 import Header from './components/Header';
-
+import Tags from './components/Tags';
 
 const App: FC = () => {
   return (
@@ -14,7 +14,16 @@ const App: FC = () => {
         ]}
       />
       <div className='w-3/12 mx-auto'>
-
+        <Tags
+          tags={[
+            { name: 'React', path: '/tags/react' },
+            { name: 'TypeScript', path: '/tags/typescript' },
+            { name: 'Next.js', path: '/tags/next.js' },
+            { name: 'Tailwind CSS', path: '/tags/tailwind-css' },
+            { name: 'Vercel', path: '/tags/vercel' },
+            { name: 'GitHub', path: '/tags/github' }
+          ]}
+        />
       </div>
     </>
   );
---
+++ src/components/Tags.tsx
@@ -0,0 +1,33 @@
+import { FC } from 'react';
+import { Link } from 'react-router-dom';
+
+interface Props {
+  tags: {
+    name: string;
+    path: string;
+  }[];
+}
+
+const Tags: FC<Props> = ({ tags }) => {
+  return (
+    <div className='bg-white'>
+      <h1 className='text-md text-center py-4 font-bold'>
+        タグ
+      </h1>
+      <div className='px-4 flex flex-wrap items-center justify-start'>
+        {tags.map((tag) => (
+          <Link
+            key={tag.path}
+            to={tag.path}
+          >
+            <p className='bg-gray-200 inline-block rounded-full px-3 py-1 text-sm mr-2 mb-2 hover:underline'>
+              {tag.name}
+            </p>
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Tags;
