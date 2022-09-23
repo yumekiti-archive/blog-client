b2a22823985406478fca901a2c1cc24e33da9954
yumekiti <yumekiti120440@gmail.com>
2022-09-23 19:44:43 +0900

    `add` Introduce

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Routes, Route, Link } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import Header from './components/Header';
+import Introduce from './components/Introduce';
 
 const App: FC = () => {
   return (
@@ -15,12 +16,21 @@ const App: FC = () => {
           { name: 'About', path: '/about' }
         ]}
       />
-      <Routes>
+      <div className='w-3/12 mx-auto'>
+        <Introduce
+          name='ゆめきち'
+          body='専門学校生です。主にインフラを勉強しています。'
+          links={[
+            { name: 'Twitter', path: 'https://twitter.com/yumekiti1204', img: 'https://img.icons8.com/ios-filled/50/000000/twitter.png' },
+            { name: 'GitHub', path: 'https://github.com/yumekiti', img: 'https://img.icons8.com/ios-filled/50/000000/github.png' }
+          ]}
+        />
+      </div>
+
+      {/* <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-      </Routes>
-      <Link to="/">Home</Link>
-      <Link to="/about">About</Link>
+      </Routes> */}
     </>
   );
 }
---
+++ src/components/Introduce.tsx
@@ -0,0 +1,47 @@
+import { FC } from 'react';
+
+interface Props {
+  name: string;
+  body: string;
+  links: { name: string; path: string; img: string }[];
+}
+
+const Introduce: FC<Props> = ({ name, body, links }) => {
+  return (
+    <div className='bg-white'>
+      <h1 className='text-md text-center py-4'>
+        自己紹介
+      </h1>
+      <img
+        src="https://pbs.twimg.com/profile_images/1492151996857860097/MRnAqw5h_400x400.png"
+        alt="アイコン"
+        className='w-32 h-32 mx-auto rounded-full object-cover'
+      />
+      <p className='text-center font-bold'>
+        {name}
+        ゆめきち
+      </p>
+      <div className='p-4'>
+        <p className='text-center text-sm'>
+          {body}
+          専門学校生です。<br/>主にインフラを勉強しています。
+        </p>
+        <ul className='flex items-center justify-end mt-4'>
+          {links.map((link) => (
+            <li className='mx-2'>
+              <a href={link.path}>
+                <img
+                  src={link.img}
+                  alt={link.name}
+                  className='w-6 h-6'
+                />
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default Introduce;
