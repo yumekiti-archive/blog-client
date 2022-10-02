f9f2172fdb02618ee2930c97f8b46719c38c21a7
yumekiti <yumekiti120440@gmail.com>
2022-10-02 23:55:16 +0900

    `fix` interfaces

--- src/libs/interfaces/knowledge.ts
+++ src/libs/interfaces/knowledge.ts
@@ -1,7 +1,13 @@
+import Category from "./category";
+import Tag from "./tag";
+
 export default interface Knowledge {
   id: number;
   img: string;
   title: string;
   subtitle: string;
   date: string;
+  path: string;
+  category: Category;
+  tags: Tag[];
 }
