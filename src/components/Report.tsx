0d9bb2b8d6daea3fb5dd92b2e309af14235a5163
yumekiti <yumekiti120440@gmail.com>
2022-10-03 01:41:39 +0900

    `refactoring` ReportComponent

--- src/components/Report.tsx
+++ src/components/Report.tsx
@@ -1,60 +1,49 @@
 import { FC } from "react";
 import { Link } from "react-router-dom";
+import Report from "../libs/interfaces/report";
 
 interface Props {
-  report: {
-    title: string,
-    date: string,
-    body: string,
-    category: {
-      id: number,
-      name: string,
-    },
-    tags: {
-      id: number,
-      name: string,
-    }[]
-  },
+  report: Report
 }
 
-const Report: FC<Props> = ({ report }) => {
+const ReportComponent: FC<Props> = ({ report }) => {
   return (
-    <>
-      <div className="bg-cyan-100 rounded">
-        <div className="flex flex-col items-center justify-center">
-          <h1 className="text-xl text-left mt-8 mx-10">{report.title}</h1>
-          <p className="text-sm text-center p-4">{report.date}</p>
-        </div>
-        <div className="p-4">
-          <div className="bg-white min-h-screen rounded">
-            <div className="w-full flex justify-end items-center">
-              <p className="text-sm px-3 py-1 bg-cyan-100 rounded-full mx-2 my-2 hover:bg-cyan-200 cursor-pointer">
-                <Link to={`/category/${report.category.id}`}>
-                  {report.category.name}
-                </Link>
-              </p>
-            </div>
-            {/* markdown */}
-            <div className="p-4 min-h-screen">
-              {report.body}
-            </div>
-            <div className="flex justify-start items-center flex-wrap bg-white rounded">
-              {report.tags.map((tag) => (
+    <div className="bg-cyan-100 rounded">
+      <div className="block p-4 text-center">
+        <h1 className="text-xl">{report.title}</h1>
+        <p className="text-xs">{report.date}</p>
+      </div>
+      <div className="px-4 pb-4">
+        <div className="bg-white min-h-screen rounded">
+          <div className="w-full flex justify-end items-center p-2">
+            <p className="text-sm px-3 py-1 bg-cyan-100 rounded-full hover:bg-cyan-200 cursor-pointer">
+              <Link to={`/category/${report.category.id}`}>
+                {report.category.name}
+              </Link>
+            </p>
+          </div>
+          {/* markdown */}
+          <div className="p-4 min-h-screen">
+            {report.body}
+          </div>
+          <div className="flex justify-start items-center flex-wrap bg-white">
+            {report.tags.map((tag) => (
+              <div className="p-2">
                 <Link
                   key={tag.id}
                   to={'/tag/' + tag.id}
                 >
-                  <p className='text-sm bg-gray-200 inline-block rounded-full px-3 py-1 cursor-pointer ml-2 my-2 hover:underline'>
+                  <p className='text-sm bg-gray-200 rounded-full px-3 py-1 cursor-pointer hover:underline'>
                     {tag.name}
                   </p>
                 </Link>
-              ))}
-            </div>
+              </div>
+            ))}
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
-export default Report;
+export default ReportComponent;
