21b84cd5020e26687d5997e4446eb6bc502d9db0
yumekiti <yumekiti120440@gmail.com>
2022-10-03 00:26:19 +0900

    `refactoring` CategoriesComponent

--- src/components/Categories.tsx
+++ src/components/Categories.tsx
@@ -1,32 +1,30 @@
 import { FC } from 'react';
 import { Link } from 'react-router-dom';
+import Category from '../libs/interfaces/category';
 
 interface Props {
-  categories: {
-    id: number;
-    name: string;
-  }[];
+  categories: Category[];
 }
 
-const Categories: FC<Props> = ({ categories }) => {
+const CategoriesComponent: FC<Props> = ({ categories }) => {
   return (
     <div className='bg-white rounded'>
       <h1 className='text-xl text-center py-4'>
         カテゴリー
       </h1>
-      <div className='px-4 flex flex-wrap'>
+      <div className='px-4 flex flex-wrap items-center justify-center'>
         {categories.map((category) => (
           <Link
             key={category.id}
             to={'/category/' + category.id}
-            className='block pb-4 w-1/3 lg:w-full'
+            className='py-2 w-1/3 lg:w-full'
           >
-            <p className='text-sm text-center cursor-pointer hover:underline flex items-center justify-center'>
-              <span className='flex items-center justify-center'>
-                <span className='w-3 h-3 bg-cyan-100 rounded-full mr-2'></span>
-              </span>
-              {category.name}
-            </p>
+            <div className='cursor-pointer hover:underline flex items-center justify-center'>
+              <div className='flex items-center justify-center'>
+                <span className='w-3 h-3 bg-cyan-100 rounded-full mr-2' />
+              </div>
+              <span>{category.name}</span>
+            </div>
           </Link>
         ))}
       </div>
@@ -34,4 +32,4 @@ const Categories: FC<Props> = ({ categories }) => {
   );
 }
 
-export default Categories;
+export default CategoriesComponent;
