05c9b7dc33fa64e484a49077e46a1330db4164b7
yumekiti <yumekiti120440@gmail.com>
2022-12-11 23:24:01 +0900

    `refactoring` style

--- src/components/Knowledges.tsx
+++ src/components/Knowledges.tsx
@@ -91,17 +91,19 @@ const KnowledgesComponent: FC<Props> = ({ pageSize, find }) => {
                         <p className='text-sm overflow-scroll overflow-hidden'>
                           {knowledge.attributes.createdAt.split('T')[0].split('-').join('/')}
                         </p>
-                        <p className='text-xl overflow-scroll overflow-hidden'>{knowledge.attributes.title}</p> 
-                        {knowledge.attributes.content ? (
-                          <p className='text-sm overflow-scroll overflow-hidden'>{knowledge.attributes.content}</p>
-                        ) : (
-                          <p className='text-sm overflow-scroll overflow-hidden'>未開拓</p>
-                        )}
+                        <div className='overflow-scroll overflow-hidden'>
+                          <p className='text-xl'>{knowledge.attributes.title}</p> 
+                          {knowledge.attributes.content ? (
+                            <p className='text-sm'>{knowledge.attributes.content}</p>
+                          ) : (
+                            <p className='text-sm'>未開拓</p>
+                          )}
+                        </div>
                       </div>
                     </div>
                   </a>
                   <div className='flex justify-end items-center'>
-                    <p className='text-sm px-3 py-1 bg-cyan-100 rounded-full mx-2 my-2 hover:bg-cyan-200 cursor-pointer whitespace-nowrap'>
+                    <div className='text-sm px-3 py-1 bg-cyan-100 rounded-full mx-2 my-2 hover:bg-cyan-200 cursor-pointer whitespace-nowrap'>
                       {knowledge.attributes.category.data ? (
                         <Link to={`/categories/${knowledge.attributes.category.data.id}`}>
                           {knowledge.attributes.category.data.attributes.name}
@@ -109,16 +111,17 @@ const KnowledgesComponent: FC<Props> = ({ pageSize, find }) => {
                       ) : (
                         <p>未分類</p>
                       )}
-                    </p>
+                    </div>
                     <div className='overflow-scroll overflow-hidden flex'>
                       {knowledge.attributes.tags.data &&
                         knowledge.attributes.tags.data.map((tag) => (
-                          <p
-                            className='text-sm bg-gray-200 inline-block rounded-full px-3 py-1 cursor-pointer mr-2 my-2 hover:underline whitespace-nowrap'
-                            key={tag.id}
-                          >
-                            <Link to={'/tags/' + tag.id}>{tag.attributes.name}</Link>
-                          </p>
+                          <Link to={'/tags/' + tag.id} key={tag.id}>
+                            <p
+                              className='text-sm bg-gray-200 inline-block rounded-full px-3 py-1 cursor-pointer mr-2 my-2 hover:underline whitespace-nowrap'
+                            >
+                              {tag.attributes.name}
+                            </p>
+                          </Link>
                         ))}
                     </div>
                   </div>
@@ -145,28 +148,28 @@ const KnowledgesComponent: FC<Props> = ({ pageSize, find }) => {
         <div className='flex items-center justify-center mt-4'>
           {page > 1 ? (
             <button onClick={() => setPage(page - 1)} className='mr-2'>
-              <p className='w-8 h-8 bg-white rounded-full flex items-center justify-center text-sm hover:bg-cyan-100'>
-                <div className='flex justify-center items-center'>
+              <p className='w-8 h-8 card-color rounded-full flex items-center justify-center text-sm hover:bg-cyan-100'>
+                <span className='flex justify-center items-center'>
                   <i className='las la-angle-left'></i>
-                </div>
+                </span>
               </p>
             </button>
           ) : (
-            <p className='w-8 h-8 bg-white rounded-full flex items-center justify-center text-sm mr-2' />
+            <p className='w-8 h-8 card-color rounded-full flex items-center justify-center text-sm mr-2' />
           )}
           <p className='text-md mr-2'>
             {page} / {total}
           </p>
           {page < total ? (
             <button onClick={() => setPage(page + 1)} className='mr-2'>
-              <p className='w-8 h-8 bg-white rounded-full flex items-center justify-center text-sm hover:bg-cyan-100'>
-                <div className='flex justify-center items-center'>
+              <p className='w-8 h-8 card-color rounded-full flex items-center justify-center text-sm hover:bg-cyan-100'>
+                <span className='flex justify-center items-center'>
                   <i className='las la-angle-right'></i>
-                </div>
+                </span>
               </p>
             </button>
           ) : (
-            <p className='w-8 h-8 bg-white rounded-full flex items-center justify-center text-sm mr-2' />
+            <p className='w-8 h-8 card-color rounded-full flex items-center justify-center text-sm mr-2' />
           )}
         </div>
       </>
